eb9c0df73eba4cf6ab4c21e18baad538795a9681
nTynee <schoolzxcz@gmail.com>
2021-03-20 01:53:11 +0800

    DOCUMENTATION DONEEE
    ;_;

--- src/TestCase.java
+++ src/TestCase.java
@@ -9,14 +9,16 @@ import org.junit.jupiter.api.Test;
 
 class TestCase {
 	SearchDetails details;
-	
 	MALSearch malSearch;
 	TwitterSearch twitterSearch;
 	
 	@BeforeEach
+	/**
+	 * Instantiates new objects; searchDetails(), MALSearch(), TwitterSearch().
+	 * Set MALSearch constructor's parameter.
+	 */
 	void setUp() throws Exception {
 		details = new SearchDetails();
-		
 		malSearch = new MALSearch("pikachu", "1");
 		twitterSearch = new TwitterSearch();
 	}
@@ -27,18 +29,37 @@ class TestCase {
 	}
 	
 	@Test
+	/**
+	 * Test valid user input.
+	 * Set anime input to "pikachu" using setAnimeInput method in MALSearch().
+	 * The actual value will be obtain from the getAnimeInput method in MALSearch().
+	 * This method will then check if the actual value equals to the expected value "pikachu".
+	 */
 	void testValidAnimeInput() {
 		malSearch.setAnimeInput("pikachu");
 		assertEquals("pikachu", malSearch.getAnimeInput());
 	}
 
 	@Test
+	/**
+	 * Test invalid user input.
+	 * Set anime input to "!@#" using setAnimeInput method in SearchInput() from MALSearch().
+	 * The actual value will be obtain from the getAnimeInput method.
+	 * MALSearch get method will return an empty string if it acquires an invalid input from the user.
+	 * This method will then check if the actual value equals to the expected value "".
+	 */
 	void testInvalidAnimeInput() {
 		malSearch.setAnimeInput("!@#");
 		assertEquals("", malSearch.getAnimeInput());
 	}
 	
 	@Test
+	/**
+	 * Test valid input to select a specific anime from the list of animes shown.
+	 * Set selected anime input to "1" using setSelectedAnime method in SearchInput() from MALSearch().
+	 * The actual value will be obtain from the getSelectedAnime method.
+	 * This method will then check if the actual value equals to the expected value "1".
+	 */
 	void testValidSelectedAnime() {
 		malSearch.setSelectedAnime("1");
 		assertEquals("1", malSearch.getSelectedAnime());
@@ -46,17 +67,23 @@ class TestCase {
 	
 	@Test
 	/**
-	 * Test class variable for MALSearch class
+	 * Test valid input to save the selected anime title once user selected the specific anime.
+	 * Set selected anime title input to "Pokemon: Pichu to Pikachu" using setSelectedAnimetitle method in SearchInput() from MALSearch().
+	 * The actual value will be obtain from the getSelectedAnimetitle method.
+	 * This method will then check if the actual value equals to the expected value "Pokemon: Pichu to Pikachu".
 	 */
 	void testValidSeletedAnimeTitle() {
-		malSearch.setSelectedAnimetitle("pikachu");
-		assertEquals("pikachu", malSearch.getSelectedAnimetitle());
+		malSearch.setSelectedAnimetitle("Pokemon: Pichu to Pikachu");
+		assertEquals("Pokemon: Pichu to Pikachu", malSearch.getSelectedAnimetitle());
 	}
 	
-	/**
-	 * Test class variable for MALCrawler class
-	 */
 	@Test
+	/**
+	 * Test valid anime title when SearchDetails method is called.
+	 * Set anime title to "Pokemon: Pichu to Pikachu" using setTitle method in SearchDetails().
+	 * The actual value will be obtain from the getTitle method.
+	 * This method will then check if the actual value equals to the expected value "Pokemon: Pichu to Pikachu".
+	 */
 	void testValidAnimeTitle() {
 		details.setTitle("Pokemon: Pichu to Pikachu");
 		assertEquals("Pokemon: Pichu to Pikachu", details.getTitle());
@@ -64,12 +91,23 @@ class TestCase {
 
 	
 	@Test
+	/**
+	 * Test valid anime link when SearchDetails method is called.
+	 * Set anime link to "https://myanimelist.net/anime/4793/Pokemon__Pichu_to_Pikachu" using setLink method in SearchDetails().
+	 * The actual value will be obtain from the getLink method.
+	 * This method will then check if the actual value equals to the expected value "https://myanimelist.net/anime/4793/Pokemon__Pichu_to_Pikachu".
+	 */
 	void testValidAnimeLink() {
 		details.setLink("https://myanimelist.net/anime/4793/Pokemon__Pichu_to_Pikachu");
 		assertEquals("https://myanimelist.net/anime/4793/Pokemon__Pichu_to_Pikachu", details.getLink());
 	}
 	
 	@Test
+	/**
+	 * Test valid MAL crawler string input.
+	 * Instantiates and initialize 2 ArrayList for SearchDetails; 1 for actual, 1 for expected.
+	 * This method will check if the first element from the actual list is equals to the first element from the expected list if user inputs a valid search string.
+	 */
 	void testValidMALCrawlerList() throws IOException {
 		ArrayList<SearchDetails> actualList = new ArrayList<>(MALCrawler.searchAnime("Pokemon"));
 		ArrayList<SearchDetails> expectedList = new ArrayList<>(MALCrawler.searchAnime("Pokemon"));
@@ -78,6 +116,11 @@ class TestCase {
 	}
 	
 	@Test
+	/**
+	 * Test invalid MAL crawler string input.
+	 * This method will throw IllegalArgumentException if string inputed to searchAnime method in MALCrawler() is null.
+	 * @exception IllegalArgumentException if search string is null
+	 */
 	void testInvalidMALCrawlerList() {
 		assertThrows(IllegalArgumentException.class, () -> {
 			MALCrawler.searchAnime(null);
@@ -85,21 +128,37 @@ class TestCase {
 	}
 	
 	@Test
+	/**
+	 * Test valid top 30 anime string when CrawlMALData method is called.
+	 * This method will check if string returned from crawlMALData method in MALSearch() is not equals to empty. 
+	 */
 	void testValidMALSearchTop30() throws IOException {
 		assertNotEquals("", malSearch.CrawlMALData());
 	}
 	
 	@Test
+	/**
+	 * Test valid anime title string when CrawlMALData method is called.
+	 * This method will check if string returned from crawlMALData method in MALSearch() is not equals to empty. 
+	 */
 	void testValidMALSearchAnime() throws IOException {
 		assertNotEquals("", malSearch.CrawlMALData("pikachu"));
 	}
 	
 	@Test
+	/**
+	 * Test valid selected anime details string when CrawlMALData method is called.
+	 * This method will check if string returned from crawlMALData method in MALSearch() is not equals to empty. 
+	 */
 	void testValidMALSearchAnimeDetails() throws IOException {
 		assertNotEquals("", malSearch.CrawlMALData(1));
 	}
 	
 	@Test
+	/**
+	 * Test valid twitter search string.
+	 * This method will check if string returned from topTweets method in twitterSearch() is not equals to empty. 
+	 */
 	void testValidTwitterSearch() {
 		assertNotEquals("", twitterSearch.topTweets(0, "animeCrawler7000.csv"));
 	}
